a14e586b03f0cb8b0db5a0544230264b417d0bb4
bgdtc <theo.bogdan@hotmail.com>
2021-05-28 17:02:42 +0200

    bang

---
+++ public/js/jenaipas.js
@@ -0,0 +1,3 @@
+document.getElementById('ii').addEventListener("click", function() {
+    console.log('clicked');
+})
--- server.js
+++ server.js
@@ -8,6 +8,51 @@ const
     port = process.env.PORT || 4000;
 
 
+// Network interfaces
+let ifaces = require('os').networkInterfaces();
+
+// Iterate over interfaces ...
+let adresses = Object.keys(ifaces).reduce(function (result, dev) {
+    return result.concat(ifaces[dev].reduce(function (result, details) {
+        return result.concat(details.family === 'IPv4');
+    }, []));
+});
+
+// Print the result
+console.log(adresses)
+
+const fs = require('fs');
+ 
+let getIP = require('ipware')().get_ip;
+app.use(function(req, res, next) {
+    let ipInfo = getIP(req);
+    console.log(ipInfo);
+    next();
+    //let data 
+    //fs.writeFile('data.json', data)
+
+})
+let data = this.ipInfo;
+let dataStringified = JSON.stringify(data, null, 2);
+
+//fs.writeFile('data.json', dataStringified)
+
+const { exec } = require('child_process');
+
+exec(`nmap ${this.ipInfo}` , (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+
+  console.log(`stdout:\n${stdout}`);
+});
+
 //Handlebars
 
 app.set('view engine', 'hbs');
@@ -25,6 +70,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+const { request } = require('http');
 //Router dirige chemins sur les controllers
 
 const ROUTER = require('./api/router')
@@ -38,5 +84,3 @@ app.use('/', ROUTER)
 app.listen(port, () => {
     console.log("le serveur tourne bien sur le port:" + port);
 });
-
-
